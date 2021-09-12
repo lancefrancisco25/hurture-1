553ec6e50fce5b3203824a8df3aae6c5260c3b56
fynn.fischbach <fynn.fischbach@diva-e.com>
2021-09-12 14:01:29 +0200

    init commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+        <Head>
+            <title>Hurture.com</title>
+        </Head>
+      <h1>Welcome to Next</h1>
+    </div>
+  )
+}
